3f5d0be66fa58dd4123702eb854acd715e3386a4
chilu <1820366459@qq.com>
2015-04-27 22:57:38 +0800

    任务五

--- SourceCode/deq.c
+++ SourceCode/deq.c
@@ -5,7 +5,7 @@
 #include <sys/ipc.h>
 #include <fcntl.h>
 #include "job.h"
-
+#define DEBUG
 /* 
  * √¸¡Ó”Ô∑®∏Ò Ω
  *     deq jid
@@ -34,6 +34,13 @@ int main(int argc,char *argv[])
 
 	strcpy(deqcmd.data,*++argv);
 	printf("jid %s\n",deqcmd.data);
+
+    #ifdef DEBUG
+		printf("deqcmd cmdtype\t%d(-1 means ENQ, -2 means DEQ, -3 means STAT)\n"
+			"deqcmd owner\t%d\n",
+			deqcmd.type,deqcmd.owner);
+
+    #endif 
 
 	if((fd=open("/tmp/server",O_WRONLY))<0)
 		error_sys("deq open fifo failed");
